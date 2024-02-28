b51608fe5e3ecd5d469d5f43864b2e1ccc3cd995
vasu412 <vasusingla44@gmail.com>
2024-02-28 12:14:06 +0530

    dom manipulation

---
+++ Project1/script.js
@@ -0,0 +1,9 @@
+const ele = document.getElementById("div");
+ele.style.backgroundColor = "yellow";
+ele.style.margin = "20px";
+ele.style.padding = "10px";
+ele.style.fontSize = "18px";
+ele.style.fontWeight = "bold";
+ele.style.height = "200px";
+ele.style.width = "300px";
+ele.style.color = "blue";
