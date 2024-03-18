5d5bddbd8abf1de4de502b348e6eebf86d486adf
vasu412 <vasusingla44@gmail.com>
2024-03-18 17:14:44 +0530

    new project

--- Project9/script.js
+++ Project9/script.js
@@ -1,8 +1,30 @@
-let body = document.querySelector('.body')
-for(let i=0;i<100;i+=1){
-    let div = document.createElement('div');
-    div.innerHTML="|";
-    div.style.position = 'absolute'
-    div.style.rotate = `${i}deg`
-    body.appendChild(div);
-}
+let input = document.querySelector('.input')
+let box = document.querySelector('.box')
+let button = document.querySelector('.button')
+let count =0;
+button.addEventListener("click" ,()=>{
+    if(count>0){
+        box.innerText = '';
+    }
+    let imgs = fetch(`https://api.unsplash.com/search/collections?page=8&query=${input.value}&client_id=HgAI8o_mQF82RKKHOQgwpJUX85VsmX2sc9lvcCFVkzU`)
+    async function images(){
+        let data = await imgs
+        let res = await data.json();
+        console.log(res);
+
+        res.results.forEach(x=> {
+            let img = document.createElement('img')
+            img.style.height = '300px'
+            img.style.width = '305px'
+            img.style.marginRight = '20px'
+            img.style.marginBottom = '20px'
+            x.preview_photos.forEach((ele)=>{
+                img.src = ele.urls.full
+                box.appendChild(img);
+            })
+        });
+    }
+    images();
+    input.value = '';
+    count++;
+})
